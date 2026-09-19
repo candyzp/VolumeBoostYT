529da98093144e5ffd1bf1d17dad3b7ebcac6f80
candyzp <166943815+candyzp@users.noreply.github.com>
2026-09-19 09:25:43 -0400

    Match volume HUD blueprint animation

--- YTVolumeHUD.m
+++ YTVolumeHUD.m
@@ -1,6 +1,6 @@
 #import "YTVolumeHUD.h"
-#import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
+#import <UIKit/UIKit.h>
 #import <math.h>
 
 @interface YTVolumeHUD ()
@@ -10,6 +10,7 @@
 @property(nonatomic, strong) UILabel *percentLabel;
 @property(nonatomic, strong) UISlider *slider;
 @property(nonatomic, assign) NSInteger lastDisplayedPercent;
+@property(nonatomic, assign) NSInteger animationToken;
 @property(nonatomic, assign) BOOL interactiveMode;
 @property(nonatomic, copy) YTVolumeHUDChangeBlock changeBlock;
 @end
@@ -38,16 +39,15 @@
       [UIBlurEffect effectWithStyle:UIBlurEffectStyleSystemChromeMaterialDark];
   self.backgroundView = [[UIVisualEffectView alloc] initWithEffect:blur];
   self.backgroundView.userInteractionEnabled = NO;
-  self.backgroundView.layer.cornerRadius = 24.0f;
   self.backgroundView.layer.cornerCurve = kCACornerCurveContinuous;
   self.backgroundView.layer.masksToBounds = YES;
   self.backgroundView.layer.borderWidth = 0.7f;
   self.backgroundView.layer.borderColor =
-      [UIColor colorWithWhite:1.0f alpha:0.13f].CGColor;
+      [UIColor colorWithWhite:1.0f alpha:0.14f].CGColor;
   [self addSubview:self.backgroundView];
 
   UIImageSymbolConfiguration *iconConfig =
-      [UIImageSymbolConfiguration configurationWithPointSize:23.0f
+      [UIImageSymbolConfiguration configurationWithPointSize:22.0f
                                                        weight:UIImageSymbolWeightSemibold];
   UIImage *icon = [UIImage systemImageNamed:@"speaker.wave.2.fill"
                            withConfiguration:iconConfig];
@@ -59,15 +59,17 @@
   self.titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
   self.titleLabel.text = @"Volume Boost";
   self.titleLabel.textColor = [UIColor whiteColor];
-  self.titleLabel.font = [UIFont systemFontOfSize:14.0f weight:UIFontWeightSemibold];
+  self.titleLabel.font =
+      [UIFont systemFontOfSize:13.0f weight:UIFontWeightSemibold];
+  self.titleLabel.textAlignment = NSTextAlignmentCenter;
   self.titleLabel.adjustsFontSizeToFitWidth = YES;
   self.titleLabel.minimumScaleFactor = 0.8f;
   [self addSubview:self.titleLabel];
 
   self.percentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-  self.percentLabel.textColor = [UIColor systemBlueColor];
-  self.percentLabel.textAlignment = NSTextAlignmentRight;
-  self.percentLabel.font = [UIFont systemFontOfSize:23.0f weight:UIFontWeightBold];
+  self.percentLabel.textAlignment = NSTextAlignmentCenter;
+  self.percentLabel.font =
+      [UIFont systemFontOfSize:23.0f weight:UIFontWeightBold];
   [self addSubview:self.percentLabel];
 
   self.slider = [[UISlider alloc] initWithFrame:CGRectZero];
@@ -123,27 +125,52 @@
 #pragma clang diagnostic pop
 }
 
+- (CGRect)expandedFrameForWindow:(UIWindow *)window {
+  CGFloat width = MIN(344.0f, MAX(280.0f, window.bounds.size.width - 28.0f));
+  CGFloat y = MAX(window.safeAreaInsets.top + 8.0f, 14.0f);
+  return CGRectMake((window.bounds.size.width - width) * 0.5f, y, width, 104.0f);
+}
+
+- (CGRect)collapsedFrameForWindow:(UIWindow *)window {
+  CGFloat width = MIN(154.0f, MAX(142.0f, window.bounds.size.width * 0.38f));
+  CGFloat y = MAX(window.safeAreaInsets.top + 8.0f, 14.0f);
+  return CGRectMake((window.bounds.size.width - width) * 0.5f, y, width, 44.0f);
+}
+
+- (void)setExpandedVisuals:(BOOL)expanded {
+  self.titleLabel.hidden = !expanded;
+  self.slider.hidden = !expanded;
+  self.percentLabel.textColor =
+      expanded ? [UIColor systemBlueColor] : [UIColor whiteColor];
+  self.percentLabel.font =
+      [UIFont systemFontOfSize:expanded ? 23.0f : 15.0f
+                              weight:expanded ? UIFontWeightBold
+                                              : UIFontWeightSemibold];
+  self.backgroundView.layer.cornerRadius = expanded ? 24.0f : 22.0f;
+  [self setNeedsLayout];
+}
+
 - (void)layoutSubviews {
   [super layoutSubviews];
 
   self.backgroundView.frame = self.bounds;
 
-  CGFloat height = CGRectGetHeight(self.bounds);
-  self.iconView.frame = CGRectMake(18.0f, 19.0f, 34.0f, 34.0f);
-
-  CGFloat textX = 66.0f;
-  CGFloat rightPadding = 18.0f;
   CGFloat width = CGRectGetWidth(self.bounds);
-  CGFloat percentageWidth = 92.0f;
-  CGFloat titleWidth =
-      MAX(90.0f, width - textX - percentageWidth - rightPadding - 6.0f);
+  CGFloat height = CGRectGetHeight(self.bounds);
 
-  self.titleLabel.frame = CGRectMake(textX, 15.0f, titleWidth, 24.0f);
-  self.percentLabel.frame =
-      CGRectMake(width - percentageWidth - rightPadding, 10.0f,
-                 percentageWidth, 34.0f);
-  self.slider.frame =
-      CGRectMake(textX, height - 45.0f, width - textX - rightPadding, 31.0f);
+  if (height < 60.0f) {
+    self.iconView.frame = CGRectMake(17.0f, 10.0f, 24.0f, 24.0f);
+    self.percentLabel.frame =
+        CGRectMake(48.0f, 5.0f, MAX(74.0f, width - 62.0f), 34.0f);
+    self.titleLabel.frame = CGRectZero;
+    self.slider.frame = CGRectZero;
+    return;
+  }
+
+  self.iconView.frame = CGRectMake(20.0f, 21.0f, 34.0f, 34.0f);
+  self.titleLabel.frame = CGRectMake(68.0f, 10.0f, width - 136.0f, 22.0f);
+  self.percentLabel.frame = CGRectMake(68.0f, 31.0f, width - 136.0f, 34.0f);
+  self.slider.frame = CGRectMake(22.0f, height - 40.0f, width - 44.0f, 28.0f);
 }
 
 - (void)updateDisplayedValue:(float)value {
@@ -164,40 +191,62 @@
   if (!window)
     return;
 
-  CGFloat width = MIN(344.0f, MAX(270.0f, window.bounds.size.width - 28.0f));
-  CGFloat safeTop = window.safeAreaInsets.top;
-  CGFloat targetY = MAX(safeTop + 8.0f, 14.0f);
-  CGRect targetFrame =
-      CGRectMake((window.bounds.size.width - width) * 0.5f, targetY, width,
-                 104.0f);
-
+  NSInteger token = ++self.animationToken;
+  CGRect expandedFrame = [self expandedFrameForWindow:window];
+  CGRect collapsedFrame = [self collapsedFrameForWindow:window];
   BOOL newlyAdded = self.superview != window;
-  if (newlyAdded) {
-    [self removeFromSuperview];
-    self.frame = targetFrame;
-    self.transform =
-        CGAffineTransformMakeTranslation(0.0f, -CGRectGetMaxY(targetFrame) - 18.0f);
-    self.alpha = 0.0f;
-    [window addSubview:self];
-  } else {
-    self.frame = targetFrame;
+
+  if (!newlyAdded) {
+    [window bringSubviewToFront:self];
+    [self setExpandedVisuals:YES];
+    [UIView animateWithDuration:0.12
+                     animations:^{
+                       self.frame = expandedFrame;
+                       self.transform = CGAffineTransformIdentity;
+                       self.alpha = 1.0f;
+                       [self layoutIfNeeded];
+                     }];
+    return;
   }
 
+  [self removeFromSuperview];
+  self.frame = collapsedFrame;
+  [self setExpandedVisuals:NO];
+  self.transform =
+      CGAffineTransformMakeTranslation(0.0f, -CGRectGetMaxY(collapsedFrame) - 16.0f);
+  self.alpha = 0.0f;
+  [window addSubview:self];
   [window bringSubviewToFront:self];
-  [self setNeedsLayout];
   [self layoutIfNeeded];
 
-  [UIView animateWithDuration:newlyAdded ? 0.30 : 0.16
+  [UIView animateWithDuration:0.18
                         delay:0.0
-       usingSpringWithDamping:0.86
-        initialSpringVelocity:0.25
+       usingSpringWithDamping:0.88
+        initialSpringVelocity:0.2
                       options:UIViewAnimationOptionBeginFromCurrentState |
                               UIViewAnimationOptionAllowUserInteraction
                    animations:^{
                      self.transform = CGAffineTransformIdentity;
                      self.alpha = 1.0f;
                    }
-                   completion:nil];
+                   completion:^(BOOL finished) {
+                     if (!finished || self.animationToken != token)
+                       return;
+
+                     [self setExpandedVisuals:YES];
+
+                     [UIView animateWithDuration:0.24
+                                           delay:0.03
+                          usingSpringWithDamping:0.84
+                           initialSpringVelocity:0.15
+                                         options:UIViewAnimationOptionBeginFromCurrentState |
+                                                 UIViewAnimationOptionAllowUserInteraction
+                                      animations:^{
+                                        self.frame = expandedFrame;
+                                        [self layoutIfNeeded];
+                                      }
+                                      completion:nil];
+                   }];
 }
 
 - (void)showWithValue:(float)value {
@@ -260,29 +309,53 @@
   if (!self.superview)
     return;
 
+  NSInteger token = ++self.animationToken;
   UIWindow *window =
       [self.superview isKindOfClass:[UIWindow class]] ? (UIWindow *)self.superview
                                                        : nil;
-  CGFloat travel =
-      CGRectGetHeight(self.bounds) + (window ? window.safeAreaInsets.top : 0.0f) +
-      28.0f;
+  if (!window)
+    return;
 
-  [UIView animateWithDuration:0.24
+  CGRect collapsedFrame = [self collapsedFrameForWindow:window];
+  [self setExpandedVisuals:NO];
+
+  [UIView animateWithDuration:0.15
                         delay:0.0
                       options:UIViewAnimationOptionBeginFromCurrentState |
-                              UIViewAnimationOptionCurveEaseIn
+                              UIViewAnimationOptionCurveEaseInOut
                    animations:^{
-                     self.transform = CGAffineTransformMakeTranslation(0.0f, -travel);
-                     self.alpha = 0.0f;
+                     self.frame = collapsedFrame;
+                     [self layoutIfNeeded];
                    }
                    completion:^(BOOL finished) {
-                     if (!finished)
+                     if (!finished || self.animationToken != token)
                        return;
-                     [self removeFromSuperview];
-                     self.transform = CGAffineTransformIdentity;
-                     self.changeBlock = nil;
-                     self.interactiveMode = NO;
-                     self.userInteractionEnabled = NO;
+
+                     CGFloat travel =
+                         CGRectGetMaxY(collapsedFrame) + 18.0f;
+
+                     [UIView animateWithDuration:0.20
+                                           delay:0.04
+                                         options:UIViewAnimationOptionBeginFromCurrentState |
+                                                 UIViewAnimationOptionCurveEaseIn
+                                      animations:^{
+                                        self.transform =
+                                            CGAffineTransformMakeTranslation(0.0f,
+                                                                             -travel);
+                                        self.alpha = 0.0f;
+                                      }
+                                      completion:^(BOOL finishedUp) {
+                                        if (!finishedUp ||
+                                            self.animationToken != token)
+                                          return;
+
+                                        [self removeFromSuperview];
+                                        self.transform =
+                                            CGAffineTransformIdentity;
+                                        self.changeBlock = nil;
+                                        self.interactiveMode = NO;
+                                        self.userInteractionEnabled = NO;
+                                      }];
                    }];
 }
 
