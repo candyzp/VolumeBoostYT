ebf7565e404cf62d657fa05609f8bcff8bc424f9
candyzp <166943815+candyzp@users.noreply.github.com>
2026-09-19 09:52:25 -0400

    Add reversible HUD controls

--- YTVolumeHUD.h
+++ YTVolumeHUD.h
@@ -4,9 +4,12 @@ typedef void (^YTVolumeHUDChangeBlock)(float value);
 
 @interface YTVolumeHUD : UIView
 + (instancetype)sharedHUD;
+- (BOOL)isPresentedOrTransitioning;
 - (void)showWithValue:(float)value;
 - (void)showInteractiveWithValue:(float)value
                      changeBlock:(YTVolumeHUDChangeBlock)changeBlock;
+- (void)toggleInteractiveWithValue:(float)value
+                       changeBlock:(YTVolumeHUDChangeBlock)changeBlock;
 - (void)scheduleHideAfterDelay:(NSTimeInterval)delay;
 - (void)hide;
 @end
