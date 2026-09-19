195b3b2610a8f3e1c5f5d6587a3fe9fe404a8008
candyzp <166943815+candyzp@users.noreply.github.com>
2026-09-19 09:17:29 -0400

    Rework volume boost HUD interface

--- YTVolumeHUD.h
+++ YTVolumeHUD.h
@@ -1,8 +1,12 @@
 #import <UIKit/UIKit.h>
 
+typedef void (^YTVolumeHUDChangeBlock)(float value);
+
 @interface YTVolumeHUD : UIView
-@property (nonatomic, strong) UILabel *textLabel;
 + (instancetype)sharedHUD;
 - (void)showWithValue:(float)value;
+- (void)showInteractiveWithValue:(float)value
+                     changeBlock:(YTVolumeHUDChangeBlock)changeBlock;
+- (void)scheduleHideAfterDelay:(NSTimeInterval)delay;
 - (void)hide;
 @end
